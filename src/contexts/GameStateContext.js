eb053bca1065f00140aef4f4f21686c0d86dd2f7
Skye McLean <skyemclean87@gmail.com>
2024-01-21 11:08:39 -0800

    GameStateContext Consolidation - Changed hrefs to Link - Updated components

---
+++ src/contexts/GameStateContext.js
@@ -0,0 +1,58 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const GameStateContext = createContext();
+
+export const GameStateProvider = ({ children }) => {
+
+  const [fuel, setFuel] = useState(100);
+  const [health, setHealth] = useState(100);
+  const [credits, setCredits] = useState(250);
+
+  const updateCredits = (amount) => {
+    setCredits((prevCredits) => Math.max(0, Math.min(250,
+      prevCredits + amount)));
+  };
+
+  const updateHealth = (amount) => {
+    setHealth((prevHealth) => Math.max(0, Math.min(10000,
+       prevHealth + amount)));
+  };
+
+  const updateFuel = (amount) => {
+    setFuel((prevFuel) => Math.max(0, Math.min(10000,
+       prevFuel + amount)));
+  };
+
+  return (
+    <GameStateContext.Provider value={{
+      fuel, updateFuel,
+      health, updateHealth, credits, updateCredits
+    }}>
+      {children}
+    </GameStateContext.Provider>
+  );
+};
+
+export const useFuel = () => {
+  const context = useContext(GameStateContext);
+  if (!context) {
+    throw new Error('useFuel must be used within a GameStateProvider');
+  }
+  return context;
+};
+
+export const useCredits = () => {
+  const context = useContext(GameStateContext);
+  if (!context) {
+    throw new Error('useCredits must be used within a CreditsProvider');
+  }
+  return context;
+};
+
+export const useHealth = () => {
+  const context = useContext(GameStateContext);
+  if (!context) {
+    throw new Error('useHealth must be used within a HealthProvider');
+  }
+  return context;
+};
